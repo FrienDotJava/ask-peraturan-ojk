ea7520efbcb1cf6b2849bc2400ea2bcdd81020c5
FrienDotJava <frenfrendy3@gmail.com>
2026-04-30 14:33:22 +0700

    first commit

---
+++ backend/ingest.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+
+loaders = []
+docs_path = Path("./docs")
+for item in docs_path.iterdir():
+    loaders.append(PyPDFLoader(docs_path.name+ "/" +item.name))
+
+docs = []
+for loader in loaders:
+    docs.extend(loader.load())
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=400,
+    chunk_overlap=80,
+    separators=["\n", "\n\n", "Pasal", "Ayat", " ", ""]
+)
+
+chunks = splitter.split_documents(docs)
+
+embeddings = HuggingFaceEmbeddings(model_name="intfloat/multilingual-e5-large")
+
+vector_store = Chroma.from_documents(
+    chunks,
+    embeddings,
+    persist_directory="./chroma"
+)
+
+print(f"Indexed {len(chunks)} chunks")
